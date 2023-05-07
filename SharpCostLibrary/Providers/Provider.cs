578946f88d5055aed55f4efa5fe5745249575f21
Jacob Kofod <jacob_kofod@yahoo.com>
2023-05-07 22:16:43 +0200

    Added provider base class Added AWS Cost Explorer provider

---
+++ SharpCostLibrary/Providers/Provider.cs
@@ -0,0 +1,26 @@
+﻿using SharpCostLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCostLibrary.Providers
+{
+    public class Provider
+    {
+        public string ProviderId { get; set; }
+        public string ProviderType { get; set; }
+        public virtual string Name { get; }
+
+        public virtual Cost[] Execute(int Month, int Year)
+        {
+            return null;
+        }
+
+        public virtual string GetServiceId()
+        {
+            return null;
+        }
+    }
+}
