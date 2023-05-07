028774f34cbda40dd1c46a0b30c17bf76dbeb537
Jacob Kofod <jacob_kofod@yahoo.com>
2023-05-07 21:31:54 +0200

    Added solution and project files Added cost class

---
+++ SharpCostLibrary/Model/Cost.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpCostLibrary.Model
+{
+    public class Cost
+    {
+        // Unique id
+        public string CostId { get; set; }
+
+        // E.g. AWS or Azure
+        public string Service { get; set; }
+
+        // Resource id
+        public string ResourceId { get; set; }
+
+        // Cost Center id
+        public string CostCenterId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal Total { get; set; }
+
+        public decimal Adjustment { get; set; }
+
+        public decimal ExchangedAmount { get; set; }
+
+        // Original currency
+        public string Currency { get; set; }
+
+        // Total currency
+        public string TotalCurrency { get; set; }
+
+        public decimal? ExchangeRate { get; set; }
+
+        // Cost name is used to match the cost to a resource 
+        public string Name { get; set; }
+
+        public void Calculate()
+        {
+            if (ExchangeRate == null) ExchangeRate = 1;
+
+            ExchangedAmount = (decimal)(Amount * ExchangeRate);
+
+            Total = Adjustment * ExchangedAmount;
+        }
+    }
+}
